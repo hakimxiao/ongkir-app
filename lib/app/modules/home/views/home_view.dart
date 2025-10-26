9d0368bb9130b65faced3229a7d18d5bee5bc4f2
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-26 18:37:50 +0700

    - MENCOBA dropdown_search DI APP  -

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_print
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,15 +12,34 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          DropdownSearch<String>(
+            items: (f, cs) => ["Sumbar", 'Jabar', 'Sumsel', 'Bali'],
+            decoratorProps: DropDownDecoratorProps(
+              decoration: InputDecoration(
+                labelText: 'Pilih provinsi asal : ',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            popupProps: PopupProps.menu(
+              disabledItemFn: (item) => item == 'Sumsel',
+              fit: FlexFit.loose,
+              constraints: BoxConstraints(),
+              showSearchBox: true,
+              searchFieldProps: TextFieldProps(
+                padding: EdgeInsets.all(20),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
